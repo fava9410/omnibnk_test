1d1fec3107ac62e8498be205d86352127e1344c0
fava9410 <xxalexvargas@gmail.com>
2019-08-08 12:42:48 -0500

    heroku settings

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn omnibnk_test.wsgi --log-file -
--- omnibnk_test/settings.py
+++ omnibnk_test/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '6yv_95268yg+nq@ad8!z)ewv*gk_())aq-)&cmm#oiet7h*vh0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'omnibnk_test.urls'
